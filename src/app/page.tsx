837b2a8986a9f66a7c1e704bc0f4ef7785d9f557
Josue-Santos-Pinto <js.pinto55@gmail.com>
2024-03-27 12:46:21 -0300

    skeleton created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import { Footer } from "@/components/footer"
 import { Header } from "@/components/header"
+import { TabsSkeleton } from "@/components/products/skeleton"
+import { ProductsTab } from "@/components/products/tab"
 import { ThemeToggle } from "@/components/theme-toggle"
+import { Suspense } from "react"
 
 const Page = () => {
   return (
     <div className="w-full max-w-4xl m-auto">
       <Header />
       <div className="mx-3">
-        ...
+        <Suspense fallback={<TabsSkeleton />}>
+            <ProductsTab />
+        </Suspense>
       </div>
       <Footer />
     </div>
