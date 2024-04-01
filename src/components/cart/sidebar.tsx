8a42e2a53342d1cd0f425ab83eda56b7a7a1466e
Josue-Santos-Pinto <js.pinto55@gmail.com>
2024-04-01 10:42:29 -0300

    showing cart item added

--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -2,14 +2,28 @@ import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/co
 import { Button } from "@/components/ui/button"
 import { RocketIcon } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
+import { useCartStore } from "@/stores/cart-store"
+import { CartItem } from "./item"
 
 export const CartSidebar = () => {
+
+    const { cart } = useCartStore(state => state)
+
+    let subtotal = 0
+
+    for(let i of cart){
+        subtotal += i.quantity * i.product.price
+    }
+
     return (
         <Sheet>
             <SheetTrigger asChild>
-                <Button>
+                <Button className="relative">
                     <RocketIcon className="mr-2" />
                     <p>Carrinho</p>
+                    {cart.length > 0 &&
+                        <div className="absolute size-3 bg-red-600 rounded-full -right-1 -top-1 z-10"></div>
+                    }
                 </Button>
             </SheetTrigger>
             <SheetContent>
@@ -17,20 +31,24 @@ export const CartSidebar = () => {
                     <SheetTitle>Carrinho</SheetTitle>
                 </SheetHeader>
                 <div className="flex flex-col gap-5 my-3">
-                    ...
+                    {cart.map((item, index)=>(
+                        <CartItem key={item.product.id} item={item} />
+                    ))}
                 </div>
 
                 <Separator className="my-4" />
 
                 <div className="flex justify-between items-center text-xs">
                     <div>Subtotal:</div>
-                    <div>...</div>
+                    <div>R$ {subtotal.toFixed(2)}</div>
                 </div>
 
                 <Separator className="my-4" />
 
                 <div className="text-center">
-                    <Button>Finalizar Compra</Button>
+                    <Button
+                        disabled={cart.length === 0}
+                    >Finalizar Compra</Button>
                 </div>
                 
             </SheetContent>
